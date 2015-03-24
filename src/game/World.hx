59fc2bfd5f3270c52c4151bfe0ed9d1bd7a06b53
Harrison Lambeth <harrison@nau.edu>
2015-03-23 20:43:21 -0700

    Created a world class

---
+++ src/game/World.hx
@@ -0,0 +1,71 @@
+package game;
+
+import flash.geom.Rectangle;
+import movable.Ship;
+import menus.MenuState;
+import starling.core.Starling;
+import starling.display.Image;
+import starling.display.Sprite;
+import starling.events.EnterFrameEvent;
+
+class World extends Sprite {
+	/* The 'perfect' update time, used to modify velocities in case
+	   the game is not quite running at $frameRate */
+	static var perfectDeltaTime : Float = 1 / 60;
+	
+	private var menustate:MenuState;
+	
+	private var camera:Camera;
+	
+	private var debugMouse:Image;
+	public var playerShip:Ship;
+	public var pointImage:Image;
+	
+	public function new (menustate:MenuState) {
+		
+		super();
+		
+		this.menustate = menustate;
+		
+		//camera = new Camera(new Rectangle( -1000, -1000, 1000, 1000));
+		//this.addChild(camera);
+		
+		// {texture, maxSpeed, maxAngle}
+		playerShip = new Ship(Root.assets.getTexture("test_ship"), 4, Math.PI/124);
+		playerShip.setBreakPower(0.980);
+		playerShip.setBoatAcceleration(0.025);
+		playerShip.turnFix = false;
+		
+		playerShip.goTo(Starling.current.stage.stageWidth/2,Starling.current.stage.stageHeight/2);
+		
+		pointImage = new Image(Root.assets.getTexture("point"));
+		pointImage.width = pointImage.height = 5;
+		pointImage.pivotX = pointImage.width/2.0;
+		pointImage.pivotY = pointImage.height/2.0;
+		pointImage.x = playerShip.getGoToX();
+		pointImage.y = playerShip.getGoToY();
+		
+		addChild(playerShip);
+		addChild(pointImage);
+		
+	}
+	
+	public function update(event:EnterFrameEvent) {
+		var mouse = Root.controls.getMousePos();
+
+		if(Root.controls.isDown("break")){
+			playerShip.stopMovement();
+		}
+		
+		if(Root.controls.isDown("hold")){
+			playerShip.holdSpeed();
+		}
+		
+		var modifier = (event == null) ? 1.0 : event.passedTime / perfectDeltaTime;
+		playerShip.applyVelocity(modifier);
+		
+		//camera.moveTowards(playerShip.x, playerShip.y);
+		//camera.applyCamera(this);
+	}
+	
+}
--- src/menus/Game.hx
+++ src/menus/Game.hx
@@ -7,39 +7,20 @@ import starling.events.Touch;
 import starling.events.TouchEvent;
 import starling.events.EnterFrameEvent;
 
+import game.World;
 import utility.ControlManager.ControlAction;
 import movable.SimpleMovable;
 import movable.Ship;
 
 class Game extends MenuState {
-	/* The 'perfect' update time, used to modify velocities in case
-	   the game is not quite running at $frameRate */
-	static var perfectDeltaTime : Float = 1/60;
 	
-	private var debugMouse:Image;
-	private var playerShip:Ship;
-	private var pointImage:Image;
+	private var world:World;
 	
 	override function init() {
 		rootSprite.addChild(this);
 		
-		// {texture, maxSpeed, maxAngle}
-		playerShip = new Ship(Root.assets.getTexture("test_ship"), 4, Math.PI/124);
-		playerShip.setBreakPower(0.980);
-		playerShip.setBoatAcceleration(0.025);
-		playerShip.turnFix = false;
-		
-		playerShip.goTo(this.stage.stageWidth/2,this.stage.stageHeight/2);
-		
-		pointImage = new Image(Root.assets.getTexture("point"));
-		pointImage.width = pointImage.height = 5;
-		pointImage.pivotX = pointImage.width/2.0;
-		pointImage.pivotY = pointImage.height/2.0;
-		pointImage.x = playerShip.getGoToX();
-		pointImage.y = playerShip.getGoToY();
-		
-		addChild(playerShip);
-		addChild(pointImage);
+		world = new World(this);
+		addChild(world);
 	}
 	
 	override function awake() {
@@ -58,25 +39,14 @@ class Game extends MenuState {
 	public function onTouch( event:TouchEvent ){
 		var touch:Touch = event.touches[0];
 		if(touch.phase == "ended"){
-			playerShip.goTo(touch.globalX,touch.globalY);
-			pointImage.x = touch.globalX;
-			pointImage.y = touch.globalY;
+			world.playerShip.goTo(touch.globalX,touch.globalY);
+			world.pointImage.x = touch.globalX;
+			world.pointImage.y = touch.globalY;
 		}
 	}
 	
 	function enterFrame(event:EnterFrameEvent) {
-		var mouse = Root.controls.getMousePos();
-
-		if(Root.controls.isDown("break")){
-			playerShip.stopMovement();
-		}
-		
-		if(Root.controls.isDown("hold")){
-			playerShip.holdSpeed();
-		}
-		
-		var modifier = (event == null) ? 1.0 : event.passedTime / perfectDeltaTime;
-		playerShip.applyVelocity(modifier);
+		world.update(event);
 	}
 	
 	override function transitionIn(?callback:Void->Void) { callback(); }
